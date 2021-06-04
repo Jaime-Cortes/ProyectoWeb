93690ec30031c9b3a9cb5c14826afc50b6346b2b
Fernando Mtz <fer_f@outlook.com>
2021-06-03 23:03:06 -0500

    Plantilla pdf generado y menuAlumno eliminado

---
+++ pdf.php
@@ -0,0 +1,158 @@
+<?php
+    //Configurando pdf
+    require('libs\fpdf.php');
+    
+
+    //Se obtienen los datos de la base de datos
+    //Identidad
+    $nombre = "Nombre";
+    $paterno = "ApellidoPaterno";
+    $materno = "ApellidoMaterno";
+    $boleta = "0123456789";
+    $nacimiento = "01/01/2000";
+    $genero = "Masculino";
+    $curp = "AAAA001122BBBCCCA0";
+    //Contacto
+    $calle = "Calle y numero";
+    $colonia = "Colonia";
+    $cp = "C.P";
+    $tel = "55-55-55-55-55";
+    $email = "algo@algo.com";
+    //Procedencia
+    $procedencia = "Escuela de procedencia";
+    $entidad = "Entidad federativa";
+    $promedio = "10.00";
+    $opcion = "Primera opción";
+    //Horario
+    $fechaAplicacion = "02/02/2021";
+    $hora = "13:50";
+    $laboratorio = "16"; 
+
+
+    //Se crea el pdf
+    class PDF extends FPDF{
+        // Cabecera de página
+        function Header(){
+            $this->Image('res\IPN.png',10,8,33);
+            $this->Image('res\ESCOM.PNG',160,8,33);
+            $this->SetFont('Arial','B',15);
+            $this->Cell(80);
+            $this->Cell(30,10,'Datos del Alumno',0,0,'C');
+            $this->Ln(30);
+        }
+    }
+
+    $pdf = new PDF();
+    $pdf->AliasNbPages();
+    $pdf->AddPage();
+    //Seccion Identidad
+    $pdf->SetFont('Arial','B',12);
+    $pdf->SetFillColor(200,220,255);
+    $pdf->Cell(0,8,utf8_decode("Identidad"),0,1,'C',1);
+    $pdf->Ln(5);
+    //Datos identidad
+    $pdf->SetFillColor(255,255,255);
+    $pdf->SetFont('Arial','B',12);
+    $pdf->Cell(45,10,utf8_decode("Nombre del Alumno:"));
+    $pdf->SetFont('Arial','',12);
+    $pdf->Cell(0,10,utf8_decode("$paterno $materno $nombre"));
+    $pdf->Ln(10);
+    $pdf->SetFont('Arial','B',12);
+    $pdf->Cell(16,10,utf8_decode("Boleta:"));
+    $pdf->SetFont('Arial','',12);
+    $pdf->Cell(0,10,utf8_decode("$boleta"));
+    $pdf->Ln(10);
+    $pdf->SetFont('Arial','B',12);
+    $pdf->Cell(45,10,utf8_decode("Fecha de nacimiento:"));
+    $pdf->SetFont('Arial','',12);
+    $pdf->Cell(0,10,utf8_decode("$nacimiento"));
+    $pdf->Ln(10);
+    $pdf->SetFont('Arial','B',12);
+    $pdf->Cell(18,10,utf8_decode("Género:"));
+    $pdf->SetFont('Arial','',12);
+    $pdf->Cell(0,10,utf8_decode("$genero"));
+    $pdf->Ln(10);
+    $pdf->SetFont('Arial','B',12);
+    $pdf->Cell(15,10,utf8_decode("CURP:"));
+    $pdf->SetFont('Arial','',12);
+    $pdf->Cell(0,10,utf8_decode("$curp"));
+    $pdf->Ln(15);
+    //Seccion Contacto
+    $pdf->SetFont('Arial','B',12);
+    $pdf->SetFillColor(200,220,255);
+    $pdf->Cell(0,8,utf8_decode("Contacto"),0,1,'C',1);
+    $pdf->Ln(5);
+    //Datos Contacto
+    $pdf->SetFillColor(255,255,255);
+    $pdf->SetFont('Arial','B',12);
+    $pdf->Cell(22,10,utf8_decode("Direccion:"));
+    $pdf->SetFont('Arial','',12);
+    $pdf->Cell(0,10,utf8_decode("Calle $calle Col. $colonia CP. $cp"));
+    $pdf->Ln(10);
+    $pdf->SetFont('Arial','B',12);
+    $pdf->Cell(20,10,utf8_decode("Teléfono:"));
+    $pdf->SetFont('Arial','',12);
+    $pdf->Cell(0,10,utf8_decode("$tel"));
+    $pdf->Ln(10);
+    $pdf->SetFont('Arial','B',12);
+    $pdf->Cell(40,10,utf8_decode("Correo electrónico:"));
+    $pdf->SetFont('Arial','',12);
+    $pdf->Cell(0,10,utf8_decode("$email"));
+    $pdf->Ln(15);
+    //Seccion Procedencia
+    $pdf->SetFont('Arial','B',12);
+    $pdf->SetFillColor(200,220,255);
+    $pdf->Cell(0,8,utf8_decode("Procedencia"),0,1,'C',1);
+    $pdf->Ln(5);
+    //Datos Contacto
+    $pdf->SetFillColor(255,255,255);
+    $pdf->SetFont('Arial','B',12);
+    $pdf->Cell(52,10,utf8_decode("Escuela de procedencia:"));
+    $pdf->SetFont('Arial','',12);
+    $pdf->Cell(0,10,utf8_decode("$procedencia"));
+    $pdf->Ln(10);
+    $pdf->SetFont('Arial','B',12);
+    $pdf->Cell(40,10,utf8_decode("Entidad federativa:"));
+    $pdf->SetFont('Arial','',12);
+    $pdf->Cell(0,10,utf8_decode("$entidad"));
+    $pdf->Ln(10);
+    $pdf->SetFont('Arial','B',12);
+    $pdf->Cell(38,10,utf8_decode("Promedio escolar:"));
+    $pdf->SetFont('Arial','',12);
+    $pdf->Cell(0,10,utf8_decode("$promedio"));
+    $pdf->Ln(10);
+    $pdf->SetFont('Arial','B',12);
+    $pdf->Cell(18,10,utf8_decode("Opción:"));
+    $pdf->SetFont('Arial','',12);
+    $pdf->Cell(0,10,utf8_decode("Escom corresponde a la $opcion"));
+    $pdf->Ln(15);
+    //Seccion Horario
+    $pdf->SetFont('Arial','B',12);
+    $pdf->SetFillColor(128, 222, 234);
+    $pdf->Cell(0,8,utf8_decode("Horario"),0,1,'C',1);
+    $pdf->Ln(5);
+    //Datos horaio
+    $pdf->SetFillColor(255,255,255);
+    $pdf->SetFont('Arial','',12);
+    $pdf->MultiCell(0,5,utf8_decode("A continuación se presenta la información sobre el horario del examen, es importante revisar fecha, hora y laboratorio de aplicación."));
+    $pdf->Ln(5);
+    $pdf->SetFont('Arial','B',12);
+    $pdf->Cell(20,10,"");
+    $pdf->Cell(45,10,utf8_decode("Fecha de aplicación:"));
+    $pdf->SetFont('Arial','',12);
+    $pdf->Cell(60,10,utf8_decode("$fechaAplicacion"));
+    $pdf->SetFont('Arial','B',12);
+    $pdf->Cell(14,10,utf8_decode("Hora:"));
+    $pdf->SetFont('Arial','',12);
+    $pdf->Cell(0,10,utf8_decode("$hora"));
+    $pdf->Ln(10);
+    $pdf->SetFont('Arial','B',12);
+    $pdf->Cell(20,10,"");  
+    $pdf->Cell(28,10,utf8_decode("Laboratorio:"));
+    $pdf->SetFont('Arial','',12);
+    $pdf->Cell(0,10,utf8_decode("$laboratorio"));
+    $pdf->Ln(10);
+
+
+    $pdf->Output();
+?>
